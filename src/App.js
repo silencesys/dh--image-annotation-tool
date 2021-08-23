8469ca3114a1efdd4a92447b3cbd8a4f62292b73
Martin Rocek <martin@rocek.dev>
2021-08-23 17:18:34 +0200

    🚧 (pan-scale): allow canvas panning and scaling

--- src/App.js
+++ src/App.js
@@ -8,58 +8,120 @@ import './App.css'
 import Box from './utils/Box'
 import Path from './utils/Path'
 import config from './utils/constants/config'
+import { renderer } from './utils/transformer'
 
 const App = () => {
+  // This layer is used to store the background image
   const backgroundLayer = useRef()
-  const higlightLayer = useRef()
-  const drawingLayer = useRef()
-  const canvasWrapper = useRef()
-  const [rectCoords, setRectCoords] = useState({ x: 0, y: 0, w: 0, h: 0 })
+  // This state is used to store the current information about size of canvas
+  // and name of the file that was used as a background image.
   const [backgroundSettings, setBackgroundSettings] = useState({ lrx: 0, lry: 0, fileName: '' })
-  const [isDrawing, setIsDrawing] = useState(false)
-  const [isDragging, setisDragging] = useState(false)
-  const [selectedObject, setSelectedObject] = useState(null)
-  const [scale, setScale] = useState(1)
+
+  // This layer is used to store the user's drawings
+  const storageLayer = useRef()
+  // Store points that are used to draw a non rectangular shape.
   const [points, setPoints] = useState([])
+  // This state is used to store all objects that were drew by user.
   const [objects, setObjects] = useState([])
-  const [canvasPosition, setCanvasPosition] = useState({ x: 0, y: 0 })
+  // This state is used to higlight the currently selected object.
+  const [selectedObject, setSelectedObject] = useState(null)
+  // This layer is used for drawing.
+  const drawingLayer = useRef()
+  // This is a wrapper for the canvas element that is used for transformations.
+  const canvasWrapper = useRef()
+
+  // This state is used to store selected position for the rectangle drawing.
+  const [rectCoords, setRectCoords] = useState({ x: 0, y: 0, w: 0, h: 0 })
+  // This state is used to store the current state of drawing - eg. whether it
+  // is in progress or not.
+  const [isDrawing, setIsDrawing] = useState(false)
+  // This state is used to store the current state of dragging, so when user
+  // starts dragging the canvas does not react as if was drawing.
+  const [isDragging, setisDragging] = useState(false)
+  // Store instance of transformer renderer which is used for canvas dragging.
+  const instance = useRef()
+  // Store instance of image editor.
+  const imageEditor = useRef()
+  // Set global scale for the canvas.
+  const [scale, setScale] = useState(1)
+  // Store current state of the editor.
   const [currentAction, setCurrentAction] = useState('cursor')
-  const timer = useRef(null)
+
+  /**
+   * Register the renderer transformer
+  */
+  useEffect(() => {
+    instance.current = renderer({
+      scaleSensitivity: 50,
+      minScale: 0.1,
+      maxScale: 2,
+      element: document.getElementById('imageEditor_Canvas')
+    })
+    imageEditor.current = document.getElementById('imageEditor')
+    canvasWrapper.current.addEventListener('scalechange', (e) => {
+      setScale(e.detail.scale)
+    })
+
+    const canvas = canvasWrapper.current
+    return () => {
+      canvas.removeEventListener('scalechange', (e) => {})
+    }
+  }, [])
+  /**
+   * Scale and position the canvas when the image is loaded.
+  */
+  useEffect(() => {
+    instance.current.panTo({ originX: -(backgroundSettings.lrx / 2) + (window.innerWidth / 2), originY: -(backgroundSettings.lry / 2) + (window.innerHeight / 2), scale: 1 })
+    const scaleWidth = (window.innerWidth - 100) / backgroundSettings.lrx
+    const scaleHeight = (window.innerHeight - 100) / backgroundSettings.lry
+    const scale = Math.min(scaleWidth, scaleHeight)
+    instance.current.zoomTo({ newScale: scale, x: window.innerWidth / 2, y: window.innerHeight / 2 })
+  }, [backgroundSettings])
 
   useEffect(() => {
     // Add event handling for canvases and box reseting
     const resetObjectsOnCanvas = () => {
       setObjects(state => [...state]?.map(obj => {
-        obj.fill = 'rgba(30, 62, 111, 0.5)'
-        obj.strokeFill = 'rgba(30, 62, 111)'
-
+        obj.fill = config.fill
+        obj.strokeFill = config.strokeFill
         return obj
       }))
     }
+    // Prevent context menu from appearing.
     document.addEventListener('contextmenu', (e) => {
       e.preventDefault()
     })
+    // Move the storage canvas to front, so user can delete it's content.
     if (currentAction === 'erasing') {
-      if (higlightLayer.current) {
-        higlightLayer.current.style.zIndex = '1'
+      if (storageLayer.current) {
+        storageLayer.current.style.zIndex = '1'
       }
     } else {
-      if (higlightLayer.current) {
-        higlightLayer.current.style.zIndex = '0'
+      if (storageLayer.current) {
+        storageLayer.current.style.zIndex = '0'
       }
+      // Ensure that everything that remained is back in it's initial state.
       resetObjectsOnCanvas()
     }
-  }, [higlightLayer, drawingLayer, currentAction])
+  }, [storageLayer, currentAction])
 
+  /**
+   * Register an effect that will be triggered each time user changes the tool.
+   * This should unsure that canvas will be always clear and prepared for drawing
+   * new objects.
+   */
   useEffect(() => {
     if (points.length !== 0 && currentAction !== 'pathDrawing') {
       drawPathObject(null, true)
     }
-  }, [currentAction, points, canvasPosition])
+  }, [currentAction, points])
 
+  /**
+   * Register an effect that will be triggered each time an array of drawn objects
+   * changes.
+   */
   useEffect(() => {
-    // Redraw the boxes each time the array is updated.
-    const context = higlightLayer.current.getContext('2d')
+    const context = storageLayer.current.getContext('2d')
     context.clearRect(0, 0, drawingLayer.current.width, drawingLayer.current.height)
     objects.forEach(box => {
       const type = box.name.toLowerCase()
@@ -71,6 +133,10 @@ const App = () => {
     })
   }, [objects])
 
+  /**
+   * Register an effect that will be triggered each time user adds a new point
+   * to the canvas.
+   */
   useEffect(() => {
     // Draw points with each new point added.
     const context = drawingLayer.current.getContext('2d')
@@ -79,13 +145,6 @@ const App = () => {
       drawPoint(context, point)
     })
   }, [points])
-
-  useEffect(() => {
-    if (canvasWrapper) {
-      canvasWrapper.current.style.transformOrigin = `${canvasPosition.x}px ${canvasPosition.y}px`
-      canvasWrapper.current.style.transform = `scale(${scale})`
-    }
-  }, [backgroundSettings, scale])
 
   /**
    * Get file name of the image
@@ -98,7 +157,7 @@ const App = () => {
   }
 
   /**
-   * Choose a file from the file system.
+   * Choose a background file from the file system.
   */
   const chooseFile = () => {
     const file = document.createElement('input')
@@ -112,30 +171,31 @@ const App = () => {
           { ...state, fileName: fileName, imgUrl: result.target.result }
         ))
         drawImageInBackgroundLayer(result.target.result)
+        resetDrawings()
+        document.title = `${fileName} - Image Annotation Tool`
       }
       reader.readAsDataURL(file.files[0])
     })
   }
 
-  const drawImageInBackgroundLayer = (url, scale = null) => {
+  /**
+   * Draw image in the background layer.
+   * @param {string} url
+   * @returns {void}
+   */
+  const drawImageInBackgroundLayer = (url) => {
     console.log('loop')
     const image = new Image()
     image.src = url
     image.onload = () => {
-      let downScale = 1
       const { naturalHeight, naturalWidth } = image
-      downScale = image.naturalHeight > (window.innerHeight - 100)
-        ? Math.floor(image.naturalHeight / (window.innerHeight - 100)) * 0.02
-        : 1
-      setScale(downScale)
-
       // Set size for each layer of the drawing board.
       backgroundLayer.current.height = naturalHeight
       backgroundLayer.current.width = naturalWidth
       drawingLayer.current.height = naturalHeight
       drawingLayer.current.width = naturalWidth
-      higlightLayer.current.height = naturalHeight
-      higlightLayer.current.width = naturalWidth
+      storageLayer.current.height = naturalHeight
+      storageLayer.current.width = naturalWidth
 
       setBackgroundSettings(state => ({ ...state, lrx: naturalWidth, lry: naturalHeight }))
 
@@ -282,12 +342,18 @@ const App = () => {
     }
   }
 
+  /**
+   * Reset all drawings on all canvases.
+   */
   const resetDrawings = () => {
     setObjects(state => [])
     setPoints(state => [])
-    setCanvasPosition({ x: 0, y: 0 })
   }
 
+  /**
+   * Copy code snippet to clipboard.
+   * @param {string} text - The code to copy.
+   */
   const copyTextToClipboard = (text) => {
     if (!navigator.clipboard) {
       return
@@ -295,6 +361,9 @@ const App = () => {
     navigator.clipboard.writeText(text)
   }
 
+  /**
+   * Remove object from teh canvas.
+  */
   const removeObjectFromCanvas = () => {
     if (selectedObject) {
       setObjects(state => state.filter(object => object.id !== selectedObject.id))
@@ -302,14 +371,18 @@ const App = () => {
     }
   }
 
+  /**
+   * Higlight hovered object on the canvas.
+   * @param {Object} event - The event that triggered the highlighting.
+   */
   const higlightHoveredObjects = (e) => {
     if (currentAction === 'erasing') {
-      const context = higlightLayer.current.getContext('2d')
+      const context = storageLayer.current.getContext('2d')
       for (const [index, object] of objects.entries()) {
         if (context.isPointInPath(object, e.nativeEvent.offsetX, e.nativeEvent.offsetY)) {
           setSelectedObject(object)
           console.log(selectedObject)
-          higlightLayer.current.style.cursor = 'pointer'
+          storageLayer.current.style.cursor = 'pointer'
           return setObjects(state => {
             state[index].strokeFill = config.temporaryStrokeFill
             state[index].fill = config.temporaryFill
@@ -317,7 +390,7 @@ const App = () => {
           })
         } else if (selectedObject) {
           setSelectedObject(null)
-          higlightLayer.current.style.cursor = 'default'
+          storageLayer.current.style.cursor = 'default'
           setObjects(state => {
             state[index].strokeFill = config.strokeFill
             state[index].fill = config.fill
@@ -327,48 +400,81 @@ const App = () => {
       }
 
       if (!selectedObject) {
-        higlightLayer.current.style.cursor = 'default'
+        storageLayer.current.style.cursor = 'default'
       }
     }
   }
 
-  const handleClicks = (e) => {
-    if (currentAction === 'zoom') {
-      handleZooming(e)
-    }
-  }
-
-  const handleZooming = (e) => {
-    if (currentAction === 'zoom') {
-      // I'm unable to calculate the scale position on the canvas as the numbers
-      // sometimes wildly jumps from one to another.
-      const bounding = drawingLayer.current.getBoundingClientRect()
-      console.log(e.clientX, bounding.left)
-      console.log(e)
-      if (e.ctrlKey) {
-        const posX = -Math.round((e.clientX - bounding.left * scale))
-        const posY = -Math.round((e.clientY - bounding.top * scale))
-        console.log(posX, posY)
-        setCanvasPosition({ x: posX, y: posY })
-        setScale(state => {
-          let scale = state - 0.2
-          scale = Math.round(Math.min(Math.max(0.1, scale), 2) * 100) / 100
-          return scale
-        })
-      } else {
-        setScale(state => {
-          let scale = state + 0.2
-          scale = Math.round(Math.min(Math.max(0.1, scale), 2) * 100) / 100
-          return scale
-        })
-        const posX = -Math.round((e.clientX - bounding.left))
-        const posY = -Math.round((e.clientY - bounding.top))
-        console.log(posX, posY)
-        setCanvasPosition({ x: posX, y: posY })
+  /**
+   * Register the keydown event listener. Currently only used for changing the
+   * cursor.
+   */
+  useEffect(() => {
+    document.addEventListener('keydown', (e) => {
+      if (e.ctrlKey && currentAction === 'zoom') {
+        canvasWrapper.current.style.cursor = 'zoom-out'
       }
+    })
+    document.addEventListener('keyup', (e) => {
+      if (currentAction === 'zoom') {
+        canvasWrapper.current.style.cursor = 'zoom-in'
+      }
+    })
+
+    return () => {
+      document.removeEventListener('keydown', (e) => {})
+      document.removeEventListener('keyup', (e) => {})
+    }
+  })
+
+  /**
+   * Change cursor when tools are selected.
+   */
+  useEffect(() => {
+    const cursorMap = {
+      zoom: 'zoom-in',
+      cursor: 'grab',
+      pathDrawing: 'crosshair',
+      rectangleDrawing: 'crosshair',
+      erasing: 'default'
+    }
+    canvasWrapper.current.style.cursor = cursorMap[currentAction]
+  }, [currentAction])
+
+  /**
+   * Handle the mouse down event on the canvas wrapper.
+   * @param {Object} event - The event that triggered the event.
+   */
+  const handleClickOnCanvasWrapper = (e) => {
+    if (currentAction === 'zoom') {
+      const deltaScale = e.ctrlKey ? -10 : 10
+      instance.current.zoom({ deltaScale: deltaScale, x: e.nativeEvent.pageX, y: e.nativeEvent.pageY })
+    } else if (currentAction === 'cursor') {
+      canvasWrapper.current.style.cursor = 'grabbing'
     }
   }
 
+  /**
+   * Handle the mouse move event on the canvas wrapper.
+   * @param {Object} event - The event that triggered the movement.
+  */
+  const handleMovementOnCanvasWrapper = (e) => {
+    if (currentAction === 'cursor' && e.buttons === 1 && imageEditor.current.contains(e.target) && !isDragging) {
+      instance.current.panBy({ originX: e.movementX, originY: e.movementY })
+    }
+  }
+
+  /**
+   * Handle the mouse up event on the canvas wrapper.
+   * @param {Object} event - The event that triggered the event.
+   */
+  const handleMouseUpOnCanvasWrapper = (e) => {
+    if (currentAction === 'cursor') {
+      canvasWrapper.current.style.cursor = 'grab'
+    }
+  }
+
+  // The code snippet used in the renderer.
   const code = `<facsimile>
   <surface ulx="0" uly="0" lrx="${backgroundSettings.lrx}" lry="${backgroundSettings.lry}">
     <graphic url="/${backgroundSettings.fileName}"/>
@@ -392,47 +498,47 @@ const App = () => {
 
   return (
     <div>
-      <div className='imageEditor' onMouseDown={handleClicks}>
-        <Draggable
-          onStart={() => setisDragging(true)}
-          onStop={() => setisDragging(false)}
-          disabled={currentAction !== 'cursor'}
-          defaultPosition={{ x: 10, y: 10 }}
-          offsetParent={document.getElementsByTagName('body')[0]}
+      <div
+        id='imageEditor'
+        className={`imageEditor ${backgroundSettings.fileName ? '' : 'imageEditor__ChooseFile'}`}
+      >
+        <div
+          id='imageEditor_Canvas'
+          className='imageEditor_Canvas'
+          ref={canvasWrapper}
+          onMouseDown={handleClickOnCanvasWrapper}
+          onMouseMove={handleMovementOnCanvasWrapper}
+          onMouseUp={handleMouseUpOnCanvasWrapper}
         >
-          <div>
-            <div className={`imageEditor_Canvas ${currentAction === 'cursor' && 'cursor_Hand'}`} ref={canvasWrapper}>
-              <canvas width={400} height={400} ref={backgroundLayer}>
-                Background layer
-              </canvas>
-              <canvas
-                width={400}
-                height={400}
-                ref={higlightLayer}
-                className='fixedLayer'
-                onMouseMove={higlightHoveredObjects}
-                onMouseDown={removeObjectFromCanvas}
-              >
-                Higlights layer
-              </canvas>
-              <canvas
-                width={400}
-                height={400}
-                onMouseDown={startDrawingOrPoiting}
-                onMouseUp={drawRectangle}
-                onMouseMove={drawTemporaryRectangle}
-                onMouseLeave={drawRectangle}
-                onTouchStart={startDrawingOrPoiting}
-                onTouchEnd={drawRectangle}
-                onTouchMove={drawTemporaryRectangle}
-                ref={drawingLayer}
-                className='fixedLayer'
-              >
-                Drawing layer
-              </canvas>
-            </div>
-          </div>
-        </Draggable>
+          <canvas width={400} height={400} ref={backgroundLayer}>
+            Background layer
+          </canvas>
+          <canvas
+            width={400}
+            height={400}
+            ref={storageLayer}
+            className='fixedLayer'
+            onMouseMove={higlightHoveredObjects}
+            onMouseDown={removeObjectFromCanvas}
+          >
+            Higlights layer
+          </canvas>
+          <canvas
+            width={400}
+            height={400}
+            onMouseDown={startDrawingOrPoiting}
+            onMouseUp={drawRectangle}
+            onMouseMove={drawTemporaryRectangle}
+            onMouseLeave={drawRectangle}
+            onTouchStart={startDrawingOrPoiting}
+            onTouchEnd={drawRectangle}
+            onTouchMove={drawTemporaryRectangle}
+            ref={drawingLayer}
+            className='fixedLayer'
+          >
+            Drawing layer
+          </canvas>
+        </div>
       </div>
       <Draggable
         onStart={() => setisDragging(true)}
@@ -473,7 +579,7 @@ const App = () => {
           <button
             onClick={() => setCurrentAction('zoom')}
             className={currentAction === 'zoom' && 'toolsPane__Confirmation'}
-            title='Rectangle drawing tool, click and drag to draw a rectangle.'
+            title='Zoom tool, click to zoom-in, hold ctrl and click to zoom-out.'
           >
             <FontAwesomeIcon icon={faSearch} />
           </button>
@@ -504,7 +610,7 @@ const App = () => {
             <button onClick={chooseFile} className='primary'>
               Open file
             </button>
-            <button onClick={resetDrawings}>
+            <button onClick={resetDrawings} title='Erases everything from canvas.'>
               Reset
             </button>
             <button className='optoinsPane__ClipboardButton' onClick={() => copyTextToClipboard(code)}>
@@ -514,6 +620,9 @@ const App = () => {
           </div>
         </div>
       </Draggable>
+      <div className='footer'>
+        This project was developed by <a href='https://rocek.dev'>Martin Roček</a>, source code is available on <a href='https://github.com' target='_blank' rel='noreferrer'>GitHub</a>. The project is licensed under the EUPL license.
+      </div>
     </div>
   )
 }
